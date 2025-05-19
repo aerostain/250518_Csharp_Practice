56fe742ed69709ae13e2e0c9e0af86a395bef1dc
r1ck7 <richims026@gmail.com>
2025-05-18 22:26:24 -0500

    Init

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Csharp_Practice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        myAct(10);
+        Console.WriteLine($"\n{myFunc(10)}");
+    }
+
+    // Un Action es un delegado: encapsula un metodo void
+    public static Action<int> myAct = (x) => Console.WriteLine($"\n{DateTime.Now.AddDays(x)}\n");
+    // Un Func es un delegado: encapsula un metodo con retorno
+    public static Func<int, double> myFunc = (x) => x * Math.PI;
+
+    static void Test_4()
+    {
+        List<int> numsAleatorios = new();
+        Random numale = new();
+        for (int i = 0; i < 10; i++)
+        {
+            numsAleatorios.Add(numale.Next(0, 10));
+        }
+        numsAleatorios.ForEach(Console.WriteLine);
+    }
+
+    static void Test_1()
+    {
+        Func<int, int> myFunc = (z) => z * 2;
+
+        Console.Write($"Ingresa un numeró: ");
+        var tmp = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine($"Resultado: {myFunc(tmp)}");
+    }
+}
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Practica C#
+
+## Delegados
+
+* Un Action es un delegado: encapsula un metodo void:
+```csharp
+public static Action<int> myAct = (x) => Console.WriteLine($"\n{DateTime.Now.AddDays(x)}\n");
+```
+
+* Un Func es un delegado: encapsula un metodo con retorno:
+```csharp
+public static Func<int, double> myFunc = (x) => x * Math.PI;
+```
+
+## Foreach para listas
+
+```csharp
+List<int> numsAleatorios = new();
+Random numale = new();
+for (int i = 0; i < 10; i++)
+{
+  numsAleatorios.Add(numale.Next(0, 10));
+}
+numsAleatorios.ForEach(Console.WriteLine);
+```
+
+## Func y captura por consola
+```csharp
+Func<int, int> myFunc = (z) => z * 2;
+
+Console.Write($"Ingresa un numeró: ");
+var tmp = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"Resultado: {myFunc(tmp)}");
+```
